9df107a44b77d406c2b1b172866802a4e047b181
sileymane <diankasileymane@gmail.com>
2018-09-23 14:12:51 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -22,5 +22,5 @@ sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
-# Join peer0.org1.example.com to the channel.
+# Join peer1.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer1.org1.example.com peer channel join -b mychannel.block
